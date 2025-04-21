c0355dd07e17491ca0c9b50d5857254b07c80dd2
meyigi <faybmi7@gmail.com>
2025-04-21 18:52:44 +0600

    feat: inbox page added

---
+++ app/components/inbox/Conversations.tsx
@@ -0,0 +1,9 @@
+export default function Conversation() {
+    return (
+        <div className="px-6 py-4 cursor-pointer border border-gray-300 rounded-xl">
+            <p className="mb-6 text-xl">Daniel Kanybekov</p>
+            
+            <p className="text-[#d50027]">Go to conversation</p>
+        </div>
+    )
+}
---
+++ app/inbox/page.tsx
@@ -0,0 +1,12 @@
+import Conversation from "../components/inbox/Conversations";
+
+export default function InboxPage() {
+  return (
+    <main className="max-w-[1500px] mx-auto px-6 pb-6 space-y-4">
+      <h1 className="my-6 mb-6 text-2xl">My properties</h1>
+      <Conversation />
+      <Conversation />
+      <Conversation />
+    </main>
+  );
+}
