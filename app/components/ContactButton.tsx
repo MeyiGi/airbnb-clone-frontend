959ff35064b41e98532f287f0ca9d5389a66d707
meyigi <faybmi7@gmail.com>
2025-04-21 17:48:34 +0600

    ✨ feat: add detailed information to user profile

---
+++ app/components/ContactButton.tsx
@@ -0,0 +1,7 @@
+export default function ContactButton() {
+    return (
+        <div className="py-4 cursor-pointer hover:bg-[#d50027] transition px-6 bg-[#ff385c] text-white rounded-xl">
+            Contact
+        </div>
+    )
+}
